dafcb6935aa6fcafef34a5d55cc7164f15fa1836
evsmax <40871583+evsmax@users.noreply.github.com>
2019-05-15 02:20:14 +0400

    Update L4.hs

--- L4.hs
+++ L4.hs
@@ -1,9 +1,11 @@
 data File = DataFile String Int | Folder String [File] deriving (Eq, Show)
 
-dirAllTmp :: File -> String -> (String)
-dirAllTmp (DataFile s _) path = (dirAllTmp x (path ++ "/" ++ s)) ++ (dirAllTmp (Folder s xs) path)
+dirAllTmp :: File -> String -> [String]
+dirAllTmp (DataFile s _) path = [path ++ "/" ++ s]
+dirAllTmp (Folder s[]) path = []
+dirAllTmp (Folder s(x:xs)) path = (dirAllTmp x (path ++ "/" ++ s)) ++ (dirAllTmp (Folder s xs) path)
 
-dirAll :: File -> (String)
+dirAll :: File -> [String]
 dirAll x = dirAllTmp x ""
 
 findTmp :: String -> File -> String -> String
@@ -16,9 +18,10 @@ find :: String -> File -> String
 find x y = findTmp x y ""
 
 du :: File -> Int
-du (DataFile _ x) = xs
+du (DataFile _ x) = x
 du (Folder s (x:xs)) = (du x) + (du (Folder s xs))
+du (Folder s[]) = 0
 
 one = Folder "home" [(Folder "img" []), (Folder "films" []),(Folder "doc" [(DataFile "lab1.hs" 112), (DataFile "lab2.hs" 4)])]
 two = Folder "home" [(Folder "img" []), (Folder "films" [])]
-three = Folder "home" [(Folder "doc"  [(DataFile "lab2.hs" 4)])]
+three = Folder "home" [(Folder "doc" [(DataFile "lab2.hs" 4)])]
